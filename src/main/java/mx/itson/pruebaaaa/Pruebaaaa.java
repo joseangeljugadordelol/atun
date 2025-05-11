3e526168a9af8f559f65af10e3223e0818402306
ABCDE-SONY <ABCDE-SONY@DESKTOP-E7G4DGK>
2025-05-10 20:45:30 -0700

    hola yo del futuro

---
+++ src/main/java/mx/itson/pruebaaaa/Pruebaaaa.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.pruebaaaa;
+
+/**
+ *
+ * @author ABCDE-SONY
+ */
+public class Pruebaaaa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        //hola yo del futuro
+    }
+}
